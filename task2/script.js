bbc30fed36b2ba1e79dcd0820add7ae696c767fe
Azret-45 <azretkardanov45@gmail.com>
2024-08-21 10:29:22 +0300

    Create script.js

---
+++ task2/script.js
@@ -0,0 +1,14 @@
+const  consoleLog = document.querySelector('#consoleLog');
+consoleLog.onclick = function() {
+  alert('Метода выводит информации в консоле веб-браузера');
+}
+
+const  Alert = document.querySelector('#alert');
+Alert.onclick = function() {
+  alert('Метода выводит информации внутри всплывающего окна');
+}
+
+const  Зrompt = document.querySelector('#prompt');
+Зrompt.onclick = function() {
+  alert('Метода используется для получения данных от пользователя ');
+}
